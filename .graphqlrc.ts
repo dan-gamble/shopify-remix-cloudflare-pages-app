17b6de779b7d0e7e00fa79eeedb9ac63868871b3
Dan Gamble <dan@dangamble.co.uk>
2024-02-01 12:13:02 +0000

    Add GQL config for Jetbrains

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -4,15 +4,24 @@ import { LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApiProject, ApiType } from "@shopify/api-codegen-preset";
 import type { IGraphQLConfig } from "graphql-config";
 
+const outputDir = './app/types'
+const defaultConfig = shopifyApiProject({
+  apiType: ApiType.Admin,
+  apiVersion: LATEST_API_VERSION,
+  documents: ['./app/**/*.{js,ts,jsx,tsx}'],
+  outputDir: outputDir,
+})
+
+defaultConfig.extensions.codegen.generates[`${outputDir}/admin.types.ts`] =
+  defaultConfig.extensions.codegen.generates[`${outputDir}/admin.types.d.ts`]
+delete defaultConfig.extensions.codegen.generates[
+  `${outputDir}/admin.types.d.ts`
+  ]
+
 function getConfig() {
   const config: IGraphQLConfig = {
     projects: {
-      default: shopifyApiProject({
-        apiType: ApiType.Admin,
-        apiVersion: LATEST_API_VERSION,
-        documents: ["./app/**/*.{js,ts,jsx,tsx}"],
-        outputDir: "./app/types",
-      }),
+      default: defaultConfig,
     },
   };
 
